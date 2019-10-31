b713a1b831af33e7d06e56e7ed9cbc26db9710f2
Chris Brinkley <brinklin8r@outlook.com>
2019-10-31 10:33:07 -0700

    Created Launcher Started WPF UI

---
+++ DCTools/Launcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace DCTools {
+    class Launcher {
+        [STAThread]
+        static void Main(string[] args) {
+            UI.App app = new UI.App();
+            app.Run(new UI.MainWindow());
+
+        }
+    }
+}
