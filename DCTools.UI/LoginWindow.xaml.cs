a42d27583ba0de89d9b6319ab13f21d5b276c1fe
Chris Brinkley <brinklin8r@outlook.com>
2019-12-21 11:30:32 -0800

    Added Basic Login Window

---
+++ DCTools.UI/LoginWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DCTools.UI {
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    /// 
+    public partial class LoginWindow : Window {
+        public LoginWindow() {
+            InitializeComponent();
+            Logger.Info("Login Called.");
+        }
+
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e) {
+            Logger.Debug("Login Button Clicked.");
+
+            if (tbUN.Text != "" && tbPW.Password != "") {
+                Logger.Debug("Got a User Name and Password.");
+                this.Close();
+            } else {
+                Logger.Error("You did not enter a User Name or Password.");
+                lblResults.Content = "You did not enter a User Name or Password.";
+            }
+        }
+    }
+}
--- DCTools.UI/MainWindow.xaml.cs
+++ DCTools.UI/MainWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,26 +8,27 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DCTools.UI {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    /// 
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
             Logger.Info("Starting DCTools.");
+            showLoginWindow();
         }
 
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         User.Local lUser = new User.Local();
 
-        private void btnOK_Click(object sender, RoutedEventArgs e) {
-            Logger.Debug("OK Button Clicked.");
-            lUser.Add("Chris", User.Local.Environment.CUCBECM);
+
+        private void showLoginWindow() {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
+            lblResults.Content = "Hi!";
         }
     }
 }
